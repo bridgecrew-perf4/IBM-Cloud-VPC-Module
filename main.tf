6c255be0c81b74fefe28fe7b3043470ad06ab395
greyhoundforty <ryantiffany@fastmail.com>
2020-11-10 14:31:37 -0600

    Added Resource group as data source
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "ibm_is_vpc" "vpc" {
   name                      = var.name
   classic_access            = var.classic_access
   address_prefix_management = var.address_prefix_management
-  resource_group            = var.resource_group
+  resource_group            = data.ibm_resource_group.group.id
   tags                      = var.tags
 }
