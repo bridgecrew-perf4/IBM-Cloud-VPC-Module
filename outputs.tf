76693d9d25c5901e48840f2a515db603a1aa1a0d
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 07:19:37 -0600

    Added default_network_acl as output
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output subnets {
 output cse_source_addresses {
   value = ibm_is_vpc.vpc.cse_source_addresses
 }
+
+output default_network_acl {
+  value = ibm_is_vpc.vpc.default_security_group
+}
