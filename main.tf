9df48b2954bd8b71ce928a841924e765aece4a98
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-01-20 13:45:46 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "ibm_is_vpc" "vpc" {
+resource ibm_is_vpc vpc {
   name                      = var.name
   classic_access            = var.classic_access
   address_prefix_management = var.address_prefix_management
-  resource_group            = data.ibm_resource_group.group.id
-  tags = concat(var.tags, ["vpc"])
+  resource_group            = var.resource_group
+  tags                      = concat(var.tags, ["type:vpc"])
 }
